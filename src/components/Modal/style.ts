665182d4a2be422710e82d1135db34c33d69f23d
Makson <maksonsantoss4@gmail.com>
2023-02-14 16:08:33 -0300

    fix/correção das libs e componente ProductCard

--- src/components/Modal/style.ts
+++ src/components/Modal/style.ts
@@ -2,8 +2,8 @@ import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 
 export const ModalLink = styled(Link)`
-  background-color: #4529e6;
-  color: #ffffff;
+  background-color: var(--color-brand-1);
+  color: var(--color-white-fixed);
   padding: 5px 10px;
   border-radius: 4px;
 `;
--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,30 +1,45 @@
-import { Div, DivProfile, DivInfo } from "./styles"
+import { Li, DivProfile, DivInfo } from "./styles";
 
-const ProductCard = ({title, img, description, profileImg, name, km, year, value}: any) => {
-    return (
-        <Div>
-        <img 
-            src={img} 
-            alt={title} 
-            className="productImg"
-        />
-        <h2>{title}</h2>
-        <p className="description">{description}</p>
-        <DivProfile>
-            <img 
-            src={profileImg} 
-            alt="Imagem do usuário" 
-            className="profileImg"
-        />
-            <p>{name}</p>
-        </DivProfile>
-        <DivInfo>
-            <span>{km}</span>
-            <span>{year}</span>
-            <p>{value}</p>
-        </DivInfo>
-    </Div>
-    )
+interface IProductCard {
+  title: string;
+  productImg: string;
+  description: string;
+  profileImg: string;
+  name: string;
+  km: number;
+  year: number;
+  value: number;
 }
 
-export default ProductCard
+export const ProductCard = ({
+  title,
+  productImg,
+  description,
+  profileImg,
+  name,
+  km,
+  year,
+  value,
+}: IProductCard) => {
+  return (
+    <Li>
+      <img src={productImg} alt={title} className="productImg" />
+      <h2>{title}</h2>
+      <p className="description">{description}</p>
+      <DivProfile>
+        <img src={profileImg} alt="Imagem do usuário" className="profileImg" />
+        <p>{name}</p>
+      </DivProfile>
+      <DivInfo>
+        <span>{km} KM</span>
+        <span>{year}</span>
+        <p>
+          {value.toLocaleString("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          })}
+        </p>
+      </DivInfo>
+    </Li>
+  );
+};
