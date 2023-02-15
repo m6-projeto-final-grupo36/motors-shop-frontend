cdb081603b1a82bf9c880c9a8dd0036bf8a7d80e
Jorge Kimura <kimurajorge2001@gmail.com>
2023-02-15 19:01:09 -0300

    fix: Arrumando o card de leilao de produtos

--- src/components/AuctionCard/styles.ts
+++ src/components/AuctionCard/styles.ts
@@ -1,30 +1,38 @@
 import styled from "styled-components";
 
 export const AuctionLi = styled.li`
-  width: 735px;
-  height: 368px;
+  width: 19rem;
+  height: 24.6875rem;
 
   display: flex;
   flex-direction: column;
   position: relative;
+  justify-content: space-between;
 
   list-style: none;
   color: var(--color-white-fixed);
 
   .productImg {
-    width: 735px;
-    height: 368px;
+    width: 100%;
+    height: 100%;
 
+    object-fit: cover;
     position: absolute;
     z-index: -1;
 
     filter: brightness(30%);
+
+    img{
+      width: 100%;
+      height: 100%;
+    }
   }
 
   .timer {
-    width: 105px;
-    height: 30px;
-    margin: 25px 0px 45px 25px;
+    width: 123px;
+    height: 36px;
+    margin: 33px 0px 35px 22px; 
+
     display: flex;
     align-items: center;
     justify-content: space-evenly;
@@ -33,68 +41,122 @@ export const AuctionLi = styled.li`
     border-radius: 20px;
 
     color: var(--color-grey-0);
+  
   }
 
   .productName {
-    margin-left: 25px;
-    margin-bottom: 25px;
+    margin-left: 22px;
+    margin-bottom: 24px;
+
+    width: 275px;
 
     font-size: 20px;
     font-weight: 600;
   }
 
   .productDescription {
-    margin-left: 25px;
-    margin-bottom: 25px;
+    margin-left: 22px;
+    margin-bottom: 28px;
 
     font-weight: 300;
+    width: 275px;
   }
 
   .auctionButton {
-    height: 55px;
-    margin-top: auto;
+    width: 100%;
+    height: 3.8125rem;
+    padding: 11px 26px 12px 22px;
+
     display: flex;
     align-items: center;
     justify-content: space-between;
 
     background-color: var(--color-brand-1);
 
-    p {
-      margin: 0px 40px 0px 25px;
+    button {
+      font-size: 0.875rem;
+      font-weight: 600;
+      border: var(--color-grey-10) 0.0938rem solid;
+      border-radius: 4px;
+      padding: 19px 20px;
+      min-height: 38px;
+      max-height: 38px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      /* margin: 0px 40px 0px 25px; */
     }
   }
+
+  @media (min-width: 1024px) {
+      width: 45.9375rem;
+      height: 24.25rem;
+
+      .timer{
+        margin: 24px 0px 69px 36px;
+      }
+
+      .productName {
+        width: 658px;
+        height: 25px;
+        margin: 0 41px 16px 36px;
+      }
+
+      .productDescription {
+        width: 663px;
+        height: 28px;
+        margin: 0 36px 46px 36px;
+      }
+
+      .auctionButton {
+        height: 3.875rem;
+        padding-left: 36px;
+        padding-top: 12px;
+
+        align-items: center;
+        justify-content: flex-start;
+
+        button:first-child{
+          margin-right: 16px;
+        }
+      }
+  }
 `;
 
-export const UserContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 25px;
-  margin-bottom: 25px;
+// export const UserContainer = styled.div`
+//   display: flex;
+//   align-items: center;
+//   margin-left: 25px;
+//   margin-bottom: 25px;
 
-  .profileIcon {
-    width: 30px;
-    height: 30px;
+//   .profileIcon {
+//     width: 30px;
+//     height: 30px;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
 
-    border-radius: 150px;
-    background-color: var(--color-brand-1);
-  }
+//     border-radius: 150px;
+//     background-color: var(--color-brand-1);
+//   }
 
-  .userName {
-    margin-left: 10px;
-  }
-`;
+//   .userName {
+//     margin-left: 10px;
+//   }
+// `;
 
 export const ProductInfoContainer = styled.div`
+  margin-left: 22px;
+  margin-bottom: 33px;
+  margin-top: 24px;
+
   display: flex;
-  margin-left: 25px;
+  flex-direction: column;
 
   span {
-    margin-right: 10px;
-    padding: 5px;
+    /* margin-right: 10px; */
+    padding: 4px 8px;
 
     font-size: 14px;
     color: var(--color-brand-1);
@@ -102,9 +164,23 @@ export const ProductInfoContainer = styled.div`
     background-color: var(--color-brand-4);
     border-radius: 10px;
   }
+  span:first-child{
+    margin-right: 12px;
+  }
 
   p {
-    margin-left: auto;
-    margin-right: 40px;
+    /* margin-left: auto;
+    margin-right: 40px; */
+    margin-top: 24px;
+  }
+
+  @media (min-width: 1024px) {
+    margin: 0 41px 50px 36px;
+    flex-direction: row;
+    justify-content: space-between;
+
+    p{
+      margin-top: 0;
+    }
   }
 `;
--- src/style/global.ts
+++ src/style/global.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export default createGlobalStyle`
 *{
     font-family: 'Lexend', sans-serif;
+    box-sizing: border-box;
 }
 
 :root {
